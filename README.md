5f4ab8519cc8ccb55af9889caaf51c458aa0f275
Sean <sean.anderson.wgc+1@gmail.com>
2023-08-25 23:31:06 +0100

    moved namespace to workloads and ran standard --fix

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PodChaosMonkey
+
+Get cluster logs:
+`kind export logs C:\Users\seana\kubelogs --name=my-cluster`
+
+After creating the roleBinding, run the following command:
+`kubectl create clusterrolebinding chaos-monkey-workloads --clusterrole=chaos-monkey --serviceaccount=workloads:default`
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const k8s = require('@kubernetes/client-node');
+const k8s = require('@kubernetes/client-node')
 
-const NAMESPACE = 'application';
-const INTERVAL = 10000;
+const NAMESPACE = 'workloads'
+const INTERVAL = 10000
 
 const getRandomPodIndex = (podCount) => {
-  return Math.floor(Math.random() * podCount);
-};
+  return Math.floor(Math.random() * podCount)
+}
 
 const main = async () => {
-  const kc = new k8s.KubeConfig();
-  kc.loadFromDefault();
-  
-  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
 
   const deleteRandomPod = async () => {
     try {
-      const podsRes = await k8sApi.listNamespacedPod(NAMESPACE, undefined, "false", undefined, undefined);
-      const pods = podsRes.body.items;
-      
-      const deletablePods = pods.filter(pod => !pod.metadata.name.includes('pod-chaos-monkey'));
-      
-      //deletes pod if a suitable one exists
-      if (deletablePods.length > 0) {
-        const selectedPodIndex = getRandomPodIndex(deletablePods.length);
-        const selectedPodName = deletablePods[selectedPodIndex].metadata.name;
+      const podsRes = await k8sApi.listNamespacedPod(NAMESPACE, undefined, 'false', undefined, undefined)
+      const pods = podsRes.body.items
 
-        await k8sApi.deleteNamespacedPod(selectedPodName, NAMESPACE);
-        console.log(`Pod ${selectedPodName} in namespace ${NAMESPACE} has been deleted!`);
+      const deletablePods = pods.filter(pod => !pod.metadata.name.includes('pod-chaos-monkey'))
+
+      // deletes pod if a suitable one exists
+      if (deletablePods.length > 0) {
+        const selectedPodIndex = getRandomPodIndex(deletablePods.length)
+        const selectedPodName = deletablePods[selectedPodIndex].metadata.name
+
+        await k8sApi.deleteNamespacedPod(selectedPodName, NAMESPACE)
+        console.log(`Pod ${selectedPodName} in namespace ${NAMESPACE} has been deleted!`)
       } else {
-        console.log("No deletable pods found.");
+        console.log('No deletable pods found.')
       }
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-    
-    setTimeout(deleteRandomPod, INTERVAL);
-  };
-  
-  deleteRandomPod(); 
-};
 
-main();
+    setTimeout(deleteRandomPod, INTERVAL)
+  }
+
+  deleteRandomPod()
+}
+
+main()
