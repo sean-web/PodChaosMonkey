8f5769aac366b469daaf7baec099c8b10fd2263d
Sean <sean.anderson.wgc+1@gmail.com>
2023-08-25 21:46:12 +0100

    working build

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ const namespace = 'test'
 
 const main = async () => {
     try {
-        const podsRes = await k8sApi.listNamespacedPod(namespace, undefined, "false", undefined, undefined, "app=snowflake");
-        console.log(podsRes.body.items);
+        const podsRes = await k8sApi.listNamespacedPod(namespace, undefined, "false", undefined, undefined);
+        const pods = podsRes.body.items
+        let podNames = []
+        // gets names of all pods in the namespace
+        for (const pod of pods) {
+            if (pod.metadata && pod.metadata.name) {
+              podNames.push(pod.metadata.name);
+            }
+          }
+        console.log(podNames)
         try {
-            //TODO: currently deleting all pods in namespace, we want a random selection to be deleted
-            k8sApi.deleteCollectionNamespacedPod(namespace)
-            console.log("Pods in namespace " + namespace + "have been deleted!")
+            const podToDelete = Math.floor(Math.random() * (podNames.length -1));
+            const selectedPodName = podNames[podToDelete]
+            k8sApi.deleteNamespacedPod(selectedPodName, namespace)
+            console.log("Pod " + selectedPodName + " in namespace " + namespace + " have been deleted!")
         } catch (err) {
             console.log(err)
         }
